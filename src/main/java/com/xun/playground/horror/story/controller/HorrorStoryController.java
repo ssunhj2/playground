93de8af53cb8e8909701b55fabe8d5bd7ac3c955
ssunhj2 <wiiro017@gmail.com>
2023-01-02 16:32:49 +0900

    1. horror story theme 적용 2. horror story 게시판 dialet 이용하여 default 페이지 생성

---
+++ src/main/java/com/xun/playground/horror/story/controller/HorrorStoryController.java
@@ -0,0 +1,16 @@
+package com.xun.playground.horror.story.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 무서운이야기 목록페이지
+ * 권한체크 대상이 되는 페이지
+ */
+@Controller
+public class HorrorStoryController {
+    @GetMapping("/horror")
+    public String welcome() {
+        return "horror/story/horrorStoryList";
+    }
+}
