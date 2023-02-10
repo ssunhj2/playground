2607647f2bc9b624afbffa0105939e1f8e8e1798
ssunhj2 <wiiro017@gmail.com>
2023-02-10 21:40:14 +0900

    불필요한 import 제거

--- src/main/java/com/xun/playground/common/interceptor/AuthInterceptor.java
+++ src/main/java/com/xun/playground/common/interceptor/AuthInterceptor.java
@@ -5,10 +5,8 @@ import com.xun.playground.common.session.SessionManager;
 import com.xun.playground.common.user.dto.UserDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
